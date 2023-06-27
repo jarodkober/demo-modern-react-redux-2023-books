b9ea089fd1855641cc873e8b58a6b7a976a5f634
Jarod Kober <jarod.development@gmail.com>
2023-06-26 18:14:09 -0700

    Displayed List of Books Entered By User
    * In the App component
        * Updated handleCreateBook to use the title and randomly generated id to update the books state
        * Imported the BookList component
        * Added a class for styling
    * In the BookList component
        * Updated to accept an array of books as a prop
        * Imported the BookShow component
        * Mapped over the books array to pass down book properties as props to BookShow
    * In the BookShow component
        * Updated to accept a book object as a prop
        * Displayed the book title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
 	const [books, setBooks] = useState([]);
 
 	const handleCreateBook = (title) => {
-		console.log('Need to add a book with title:', title);
+		const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
+
+		setBooks(updatedBooks);
 	};
 
 	return (
-		<div>
+		<div className="app">
+			<BookList books={books} />
 			<BookCreate onCreate={handleCreateBook} />
 		</div>
 	);
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,16 @@
-function BookList() {
-	return <div>BookList</div>;
+import BookShow from './BookShow';
+
+function BookList({ books }) {
+	const renderedBooks = books.map((book) => {
+		return (
+			<BookShow
+				book={book}
+				key={book.id}
+			/>
+		);
+	});
+
+	return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
